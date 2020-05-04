e5d268c77346fd470d58bdb80c682b4394a16c3b
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-04 16:04:36 -0300

    Add files via upload

---
+++ condicional4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class condicional4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float num1,num2;
+		
+		
+		Scanner leitor=new Scanner(System.in);
+		System.out.print("digite um numero: ");
+		num1 =leitor.nextInt();
+		
+		System.out.print("digite um numero: ");
+		num2 =leitor.nextInt();
+	
+		if(num1>num2)
+			System.out.println(num1+"/"+num2);
+		
+		if(num1<num2)
+			System.out.println(num2+"/"+num1);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
