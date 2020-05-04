30b62b0f2cc606593cc1e1d179e9bc6ff9cd8bf5
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-04 14:33:25 -0300

    Add files via upload

---
+++ condicional3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class condicional3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float num;
+		
+		Scanner leitor=new Scanner(System.in);
+		
+		System.out.print("digite um numero: ");
+		num =leitor.nextInt();
+		
+		if (num > 0)
+		System.out.println("O numero e positivo");
+		
+		else if (num < 0)
+			System.out.println("numero negativo:");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
