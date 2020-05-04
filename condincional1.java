923bb12caec9ad2e9ef0016ded33b75ef8e57e5b
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-04 13:51:19 -0300

    Add files via upload

---
+++ condincional1.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class condincional1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num;
+		
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("digite um numero inteiro:");
+		num = leitor.nextInt();
+		
+	
+		
+		if (num>20)
+		System.out.println(num);
+		else if(num <20);
+		else; 
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
