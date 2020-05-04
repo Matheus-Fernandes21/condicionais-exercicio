a16816f9102251e24c3495e65a8667732a15a4f1
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-04 16:32:36 -0300

    Add files via upload

---
+++ condicional5.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class condicional5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num;
+	
+		
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("digite um numero:");
+		num = leitor.nextInt();
+		
+		if(num %2==0)
+			System.out.print("numero par");
+		else
+			System.out.println("numero impar");
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
