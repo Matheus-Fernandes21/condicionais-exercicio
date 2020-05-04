d11cb2d9d7908825c47482460ad3bf55bcc84b35
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-04 14:02:49 -0300

    Add files via upload

---
+++ condicional2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class condicional2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1,num2;
+		
+				
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("digite um numero:");
+		num1 = leitor.nextInt();
+		
+		System.out.println("digite um numero:");
+		num2 = leitor.nextInt();
+		
+		int soma =num1+num2;
+		
+		
+		if(soma>10)
+		System.out.println("soma: "+soma);
+		else;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
