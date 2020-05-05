747ddbb7c992728de4a3ee441aae54024296a995
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-05 00:51:25 -0300

    Add files via upload

---
+++ condicional6.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class condicional6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a,b,c;
+    
+    Scanner leitor = new Scanner(System.in);
+    
+    System.out.print("Nota da primeira avaliacao:");
+    a = leitor.nextInt();
+    
+    if(a>2) {
+    	System.out.println("impossivel");
+    }
+    else {
+    	System.out.println("Nota da segunda avalicao");
+    	b = leitor.nextInt();
+    	
+    if (b>3) {
+    	System.out.println("impossivel");
+    }
+    	else {
+    		System.out.println("Nota da terceira avaliacao");
+    		 c = leitor.nextInt();
+    		 if(c>5) {
+    			 System.out.println("impossivel");
+    		 }
+    		 else if(a+b+c<7) {
+				 System.out.println("reprovado");
+    		 }
+				 else {System.out.println("aprovado");
+				
+			
+    	
+    
+    	
+    
+    
+    	
+    		 }
+    
+    
+
+    
+  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
+
+    
+    
+    	
+    
+    
+    
+
+    
+  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
