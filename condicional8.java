abf7f7b8f75d2af4ea88164d532c64f63bb131b6
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-05 17:43:54 -0300

    Add files via upload

---
+++ condicional8.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class condicional8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leitor = new Scanner(System.in);
+	
+		String senha;
+	     senha=("AEDB");
+	     
+	     System.out.println("Digite a senha para entrar");
+	    senha=leitor.next();
+	    
+	 if(senha.equalsIgnoreCase("AEDB")) {;    
+	 System.out.println("Acesso permitido"); }
+	 else
+	 System.out.println("Acesso negado");
+	    
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
