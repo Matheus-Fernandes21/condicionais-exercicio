fbe51c5463ed7fc783dd61678289cd54f9ac7f4c
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-05 17:57:22 -0300

    Add files via upload

---
+++ condicional9.java
@@ -0,0 +1,31 @@
+  
+import java.util.Scanner;
+
+public class condicional9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		double x ;
+		
+		System.out.println("Digite o preço de compra do produto:");
+		x=leitor.nextDouble();
+		
+		if (x<=20)
+			System.out.println("Deverá vender tal produto pelo preço de "+(x*45/100+x)+"R$.");
+		else
+			System.out.println("Deverá vender tal produto pelo preço de "+(x*30/100+x)+"R$.");
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
